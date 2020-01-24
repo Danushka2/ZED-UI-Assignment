aeaa2e9345e3e191249c13f72f910fb78f9095c6
Danushka2 <dhanushkanuwan24@gmail.com>
2020-01-25 01:05:04 +0530

    nav bar completed

--- Assignment/js/main.js
+++ Assignment/js/main.js
@@ -1,8 +1,38 @@
+setOverlayHeight();
+
+
 $(document).on('ready', function () {
-    $(".regular").slick({
-      dots: true,
-      infinite: true,
-      slidesToShow: 1,
-      slidesToScroll:1
-    });
+  $(".regular").slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1
   });
+});
+
+$(document).ready(function () {
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+      $("#nav").css("background", "#29323c");
+    }
+    else {
+      $("#nav").css("background", "transparent");
+    }
+  })
+})
+
+
+
+function setOverlayHeight() {
+  bodyHeight = $(window).height();
+  bodyHeight = Number(bodyHeight);
+  document.getElementById("overlay").style.height = bodyHeight + "px";
+  console.log("setting height to:" + bodyHeight + "px");
+
+  $("#nav-btn").click(
+    function () {
+      $("#overlay").toggle();
+    }
+  );
+}
